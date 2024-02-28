264e21ed037a8ec60b3c55b34b41e6183f3661c1
bayertom <bayertom@users.noreply.github.com>
2024-02-28 11:19:03 +0100

    cv1

--- cv1_2024.m
+++ cv1_2024.m
@@ -1,6 +1,8 @@
 clc
 clear
 
+format long g
+
 %Vstupni body
 phi_WGS = 50;
 la_WGS = 15;
@@ -20,6 +22,24 @@ N_WGS = a_WGS/W_WGS;
 X_WGS =  N_WGS*cos(phir_WGS)*cos(lar_WGS);
 Y_WGS =  N_WGS*cos(phir_WGS)*sin(lar_WGS);
 Z_WGS =  N_WGS*(1-e2_WGS)*sin(phir_WGS);
+
+%Helmertova transformace, 7 parametrů
+om_x = 4.9984/3600*pi/180;
+om_y = 1.5867/3600*pi/180;
+om_z = 5.2611/3600*pi/180;
+m = 1-3.5623e-6;
+dx = -570.8285;
+dy = -85.6769;
+dz = -462.8420;
+
+%Helmertova transformace
+R = [1,om_z,-om_y;-om_z,1,om_x;om_y,-om_x,1];
+delta = [dx;dy;dz];
+xyz_wgs = [X_WGS;Y_WGS;Z_WGS];
+xyz_bess =m*R*xyz_wgs+delta;
+
+
+
 
 
 
