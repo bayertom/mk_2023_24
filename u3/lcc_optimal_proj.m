07ec6c2b705378bc63af4721143496aeed4b99f7
bayertom <bayertom@users.noreply.github.com>
2024-05-06 13:05:37 +0200

    cv11

--- u3/lcc_optimal_proj.m
+++ u3/lcc_optimal_proj.m
@@ -2,6 +2,7 @@ clc
 clear
 
 %Lambert conformal conic projection, analyze properties
+R = 6380000;
 
 %Pole
 uk = 47.54283 * pi /180;
@@ -24,5 +25,22 @@ denom = log(tan(s2/2 +pi/4)) - log(tan(s1/2 +pi/4));
 c = num/denom;
 s0 = asin(c);
 
+% Rho0
+rho_n = 2*R*cos(s0)*cos(s1)*tan(s1/2+pi/4)^c
+rho_d = c*(cos(s0)*tan(s0/2+pi/4)^c+cos(s1)*tan(s1/2+pi/4)^c)
+rho0 = rho_n/rho_d
 
+%Rho1, 2
+rho1 = rho0*(tan(s0/2 + pi/4)/tan(s1/2 + pi/4))^c
+rho2 = rho0*(tan(s0/2 + pi/4)/tan(s2/2 + pi/4))^c
+
+%Scales
+m1 = c*rho1/(R*cos(s1));
+m2 = c*rho2/(R*cos(s2));
+m0 = c*rho0/(R*cos(s0));
+
+% Distortions
+ny1 = m1 - 1;
+ny2 = m2 - 1;
+ny0 = m0 -1;
 
